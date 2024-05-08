a9ee38215a1428feb3dd61980dbc902b2676c316
Cha-JungMin <wndls075@naver.com>
2024-05-08 11:53:49 +0900

    tb-member: member sql update

--- SQL/table/tb_member.sql
+++ SQL/table/tb_member.sql
@@ -8,7 +8,7 @@ create table member (
     phone_number varchar2(13 char)      not null,
     birth_date   date                   not null,
     join_date    date                   not null,
-    grade        number(1)              not null
+    grade        number(1, 0)           not null
 );
 
 alter table member add constraint member_pk primary key ( member_id );
@@ -45,9 +45,174 @@ values ('test9', '9999', 'Lucas Thompson', '222-333-4444', '1993-01-08', '2016-0
 insert into member (id, password, name, phone_number, birth_date, join_date, grade)
 values ('test10', '1010', 'Sophia Clark', '888-999-0000', '2005-06-20', '2024-04-01', 0);
 
+create or replace package pkg_member
+is
+	function check_user(
+		p_id in varchar2,
+		p_pwd in varchar2
+	) return number;
+	
+	function get_user_id(
+        p_member_id in number
+    ) return varchar2;
+	
+	function get_user_grade(
+        p_member_id in number
+    ) return number;
+	
+	
+	
+	procedure get_login_member_no(
+		p_id in varchar2,
+		p_pwd in varchar2,
+		p_member_id out number
+	);
+	procedure get_member_info(
+		p_member_id in number,
+		p_user_info out sys_refcursor
+	);
+	procedure add_member_info(
+        p_id in varchar2,
+        p_pwd in varchar2,
+        p_name in varchar2,
+        p_phone in varchar2,
+        p_birth in varchar2,
+        p_result out number
+    );
+	procedure update_member_info(
+        p_member_id in number,
+        p_pwd in varchar2,
+        p_new_pwd in varchar2,
+        p_result out number
+    );
+	procedure delete_member_info(
+		p_member_id in number,
+		p_pwd in varchar2,
+		p_result out number
+	);
+    
+end;
+/
 
-
-
+create or replace package body pkg_member
+is
+	function check_user(
+		p_id in varchar2,
+		p_pwd in varchar2
+	) return number
+	is
+		v_count number;
+	begin
+		select count(*) into v_count from member where id = p_id and password = p_pwd;
+        if v_count > 0 then
+            return 1;
+        else
+            return 0;
+        end if;
+	end;
+	
+	function get_user_id(
+        p_member_id in number
+    ) return varchar2
+	is
+        v_user_id varchar2(50 char);
+    begin
+        select id into v_user_id from member where member_id = p_member_id;
+        return v_user_id;
+    end;
+	
+	function get_user_grade(p_member_id in number)
+    return number
+    is
+        result_value number;
+    begin
+        select grade into result_value from member where member_id = p_member_id;
+        return result_value;
+    end;
+	
+	
+	
+	procedure get_login_member_no(
+		p_id in varchar2,
+		p_pwd in varchar2,
+		p_member_id out number
+	)
+	is
+	begin
+		if check_user(p_id, p_pwd) = 1 then
+            select member_id into p_member_id from member where id = p_id and password = p_pwd;
+        else
+            p_member_id := -1;
+        end if;
+	end;
+	
+	procedure get_member_info(
+		p_member_id in number,
+		p_user_info out sys_refcursor
+	)
+	is
+	begin
+		open p_user_info for
+        select * from member where member_id = p_member_id;
+	end;
+	
+	procedure add_member_info(
+        p_id in varchar2,
+        p_pwd in varchar2,
+        p_name in varchar2,
+        p_phone in varchar2,
+        p_birth in varchar2,
+        p_result out number
+    )
+    is
+    begin
+        insert into member(id, password, name, phone_number, birth_date, join_date, grade)
+        values (p_id, p_pwd, p_name, p_phone, to_date(p_birth, 'YYYY-MM-DD'), sysdate, 0);
+        p_result := 1;
+    exception
+        when others then
+        p_result := 0;
+        raise_application_error(-20100, 'An error occurred: ' || sqlerrm);
+    end;
+	
+	procedure update_member_info(
+        p_member_id in number,
+        p_pwd in varchar2,
+        p_new_pwd in varchar2,
+        p_result out number
+    )
+	is
+    begin
+        if check_user(get_user_id(p_member_id), p_pwd) = 1 then
+            update member set password = p_new_pwd
+            where member_id = p_member_id;
+            p_result := 1;
+        else
+            p_result := -1;
+        end if;
+    end;
+	
+	procedure delete_member_info(
+		p_member_id in number,
+		p_pwd in varchar2,
+		p_result out number
+	)
+	is
+	begin
+		if check_user(get_user_id(p_member_id), p_pwd) = 1 then
+            delete from member where member_id = p_member_id;
+			p_result := 1;
+        else
+            p_result := -1;
+        end if;
+	exception
+		when others then
+		p_result := 0;
+		raise_application_error(-20101, 'An error occurred: ' || sqlerrm);
+	end;
+    
+end;
+/
 
 
 
